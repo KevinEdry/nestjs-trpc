2b39b7aa7dccfadcb34b701adf5981d68c14e46b
Kevin Orel Edry <techg9@gmail.com>
2024-07-06 16:08:42 -0700

    documentation basic setup and intro page

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,37 @@
 export default {
-  logo: <img src={"/nestjs-trpc-logo.png"}  />,
+  logo: <div className={"md:nx-inline-flex nx-gap-2 nx-items-center"}><img src={'/logo.png'} width={40} /> NestJS tRPC</div>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/KevinEdry/nestjs-trpc',
+  },
+  banner: {
+    key: '1.0-release',
+    text: (
+      <a href="https://nextra.site" target="_blank">
+        🎉 NestJS tRPC 1.0 is released. Read more →
+      </a>
+    )
+  },
+  chat: {
+    link: 'https://github.com/kevinedry/nestjs-chat',
+  },
+  docsRepositoryBase: 'https://github.com/KevinEdry/nestjs-trpc',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return (
+          <div style={{ background: 'cyan', textAlign: 'center' }}>{title}</div>
+        )
+      }
+      if (title === 'Introduction') {
+        return <>🙋🏻‍♂️&nbsp;{title}</>
+      }
+      if (title === "Setup") {
+        return <>📦️&nbsp;{title}</>
+      }
+      return <>{title}</>
+    }
   }
-  // ... other theme options
 }

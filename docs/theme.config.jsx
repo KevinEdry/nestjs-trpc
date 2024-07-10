9bfcc6e15f21dd89f73e17fc334834a6c259edf3
Kevin Orel Edry <techg9@gmail.com>
2024-07-09 18:08:22 -0700

    removed sidebar collapse

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -24,7 +24,6 @@ export default {
   },
   docsRepositoryBase: 'https://github.com/KevinEdry/nestjs-trpc',
   sidebar: {
-    toggleButton: true,
     defaultMenuCollapseLevel: 1,
   },
   nextThemes: {

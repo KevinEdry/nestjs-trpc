1988fa63c46bf523e31d4a00f6a32cbb9346b8c7
Kevin Orel Edry <techg9@gmail.com>
2024-07-21 21:45:36 -0700

    changed to yarn from pnpm

--- packages/nestjs-trpc/lib/decorators/context.decorator.ts
+++ packages/nestjs-trpc/lib/decorators/context.decorator.ts
@@ -8,12 +8,15 @@ export function Ctx(): ParameterDecorator {
   return (
     target: Object,
     propertyKey: string | symbol | undefined,
-    parameterIndex?: number | TypedPropertyDescriptor<any>
+    parameterIndex?: number | TypedPropertyDescriptor<any>,
   ) => {
     if (propertyKey != null && typeof parameterIndex === 'number') {
       const existingParams: Array<ProcedureParamDecorator> =
-        Reflect.getMetadata(PROCEDURE_PARAM_METADATA_KEY, target, propertyKey) ||
-        [];
+        Reflect.getMetadata(
+          PROCEDURE_PARAM_METADATA_KEY,
+          target,
+          propertyKey,
+        ) || [];
 
       const procedureParamMetadata: ProcedureParamDecorator = {
         type: ProcedureParamDecoratorType.Ctx,
